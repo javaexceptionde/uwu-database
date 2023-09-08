4f4db20cec5d9c211dba50d8994e02aefc4e7466
Jonathan Bull <jonathan@jbull.dev>
2023-09-08 23:50:17 +0200

    feat: Added Basic deserialisation for Commands
    Included two variables to get the command and the argument sepearte
    
    Author: Jonathan Bull

--- src/handler/command_handler.rs
+++ src/handler/command_handler.rs
@@ -1,5 +1,18 @@
 pub fn command_handler(command_args: Vec<String>) {
     for arg in command_args {
-
+        println!("{}", arg);
+        let start_index = arg.find("(");
+        let end_index = arg.rfind(")");
+        match (start_index, end_index) {
+            (Some(start_index), Some(end_index)) => {
+                let command = arg[..start_index].to_string() + &arg[end_index + 1..];
+                let args = &arg[start_index + 1..end_index];
+                println!("Command: {}", command);
+                println!("Args: {}", args);
+            }
+            _ => {
+                println!("Invalid command: {}", arg);
+            }
+        }
     }
 }
